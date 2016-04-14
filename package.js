bb166c8f6021a251e9ba73669d00a725f2c0170e
Bharath M Vasagam <bharath.manickavasagam@cchmc.org>
2016-04-14 13:41:57 -0400

    Update package.js

--- package.js
+++ package.js
@@ -3,8 +3,6 @@ Package.describe({
   version: '0.0.3',
   summary: 'SRA database query client-side API',
   git: 'https://github.com/SciDAP/meteorgeo',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
