b0a63659c5e5a0efcb93d998c21b3917d40f8521
Bharath M Vasagam <bharath.manickavasagam@cchmc.org>
2016-04-14 13:36:45 -0400

    Update package.js

--- package.js
+++ package.js
@@ -1,10 +1,8 @@
 Package.describe({
   name: 'bharath90:meteorgeo',
-  version: '0.0.2',
-  // Brief, one-line summary of the package.
+  version: '0.0.3',
   summary: 'SRA database query client-side API',
-  // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/bharath-cchmc/bharath90-meteorgeo',
+  git: 'https://github.com/SciDAP/meteorgeo',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -16,7 +14,6 @@ Package.onUse(function(api) {
   api.use("templating@1.1.7",'client');
   api.use('http@1.1.3','client');
   api.use('promise@0.6.6','client');
-  //api.use(["jquery@1.11.7"], 'client');
   api.mainModule('meteorgeo.js');
   api.export('sra','client');
 });
